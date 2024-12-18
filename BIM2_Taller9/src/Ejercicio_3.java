185eb29e9fd199d119551008d98a839a9bbe3541
CalvitaJuanJo <juanjosecalva427@gmail.com>
2024-12-17 19:29:44 -0500

    Ejercicios 3,5,7 hechos

--- BIM2_Taller9/src/Ejercicio_3.java
+++ BIM2_Taller9/src/Ejercicio_3.java
@@ -1,12 +1,80 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+import java.util.Scanner;
 
 /**
+ * Genera una aplicación que permita ingresar valores a un arreglo de cadenas.
+ * El arreglo almacena el número de elementos que el usuario lo disponga.
  *
- * @author Calva y Calva
+ * @author Juan Jose Calva
  */
 public class Ejercicio_3 {
-    
+
+    public static void main(String[] args) {
+        Scanner tcl = new Scanner(System.in);
+        System.out.println("Cuantas marcas desea Ingresar?: ");
+        int nummarcas = tcl.nextInt();
+        tcl.nextLine();
+
+        String[] marcas = new String[nummarcas];
+        int contvalidas = 0;
+
+        System.out.println("Ingresa las marcas de los vehiculos :): ");
+        for (int i = 0; i < nummarcas; i++) {
+            System.out.println("Marca [" + (i + 1) + "]: ");
+            String marca = tcl.nextLine();
+
+            char priletra = marca.charAt(0);
+            if (priletra == 'A' || priletra == 'a'
+                    || priletra == 'C' || priletra == 'c'
+                    || priletra == 'T' || priletra == 't') {
+                
+                System.out.println("Marca invalida porque empieza con '" + priletra + "'.");
+                i--;
+                
+            } else {
+                marcas[contvalidas] = marca;
+                contvalidas++;
+            }
+        }
+
+        System.out.println("Marcas de vehiculos validas:");
+        for (int i = 0; i < contvalidas; i++) {
+            System.out.println("Marca " + (i + 1) + ": " + marcas[i]);
+        }
+
+        System.out.println("Total de marcas validas: " + contvalidas);
+    }
 }
+/**
+ * run:
+ * Cuantas marcas desea Ingresar?: 
+ * 5
+ * Ingresa las marcas de los vehiculos :): 
+ * Marca [1]: 
+ * Audi
+ * Marca invalida porque empieza con 'A'.
+ * Marca [1]: 
+ * Mazda
+ * Marca [2]: 
+ * Hyundai
+ * Marca [3]: 
+ * Nissan
+ * Marca [4]: 
+ * Subaru
+ * Marca [5]: 
+ * Toyota
+ * Marca invalida porque empieza con 'T'.
+ * Marca [5]: 
+ * Chevrolet
+ * Marca invalida porque empieza con 'C'.
+ * Marca [5]: 
+ * Kia
+ * Marcas de vehiculos validas:
+ * Marca 1: Mazda
+ * Marca 2: Hyundai
+ * Marca 3: Nissan
+ * Marca 4: Subaru
+ * Marca 5: Kia
+ * Total de marcas validas: 5
+ * BUILD SUCCESSFUL (total time: 1 minute 12 seconds)
+ */
--- BIM2_Taller9/src/Ejercicio_5.java
+++ BIM2_Taller9/src/Ejercicio_5.java
@@ -1,12 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
- *
- * @author Calva y Calva
+ * Dados los siguientes arreglos
+ * @author Juan Jose Calva
  */
 public class Ejercicio_5 {
-    
+    public static void main(String[] args) {
+        
+        double[] promedio = {10, 10, 9.1, 7, 6.1, 4, 8};
+        String[] estudiantes = {"Kimberly Gonzalez", "Mark Hogan", "Teresa Martinez", 
+                                "Julia Johnson", "Mark Cook", "Jennifer Manning", "Juan Vasquez"};
+        String[] promediocualitativo = new String[7];
+
+        for (int i = 0; i < promedio.length; i++) {
+            if (promedio[i] >= 0 && promedio[i] <= 5.9) {
+                promediocualitativo[i] = "Regular";
+            } else if (promedio[i] >= 6 && promedio[i] <= 8.9) {
+                promediocualitativo[i] = "Bueno";
+            } else if (promedio[i] >= 9 && promedio[i] <= 10) {
+                promediocualitativo[i] = "Sobresaliente";
+            }
+        }
+        
+        System.out.println("Reporte de promedios");
+        for (int i = 0; i < estudiantes.length; i++) {
+            System.out.printf("%s promedio: %.2f promedio cualitativo: %s%n",
+                              estudiantes[i], promedio[i], promediocualitativo[i]);
+        }
+    }
 }
+/**
+ * run:
+ * Reporte de promedios
+ * Kimberly Gonzalez promedio: 10,00 promedio cualitativo: Sobresaliente
+ * Mark Hogan promedio: 10,00 promedio cualitativo: Sobresaliente
+ * Teresa Martinez promedio: 9,10 promedio cualitativo: Sobresaliente
+ * Julia Johnson promedio: 7,00 promedio cualitativo: Bueno
+ * Mark Cook promedio: 6,10 promedio cualitativo: Bueno
+ * Jennifer Manning promedio: 4,00 promedio cualitativo: Regular
+ * Juan Vasquez promedio: 8,00 promedio cualitativo: Bueno
+ * BUILD SUCCESSFUL (total time: 0 seconds)
+ */
